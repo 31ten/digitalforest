91fa445a0978fa1612fbb2652e0982adae8ebb01
Dotcamp <thiebault.gregoire@gmail.com>
2015-10-30 00:51:49 +0800

    grid positioning system

--- client/client.js
+++ client/client.js
@@ -1,3 +1,8 @@
+var display_canvas_width = 1200;
+var display_canvas_height = 600;
+var grid_unit_width = 40;
+var grid_unit_height = 40;
+
 window.mobilecheck = function() {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
@@ -248,6 +253,18 @@ Meteor.startup(function() {
         return Math.floor((Math.random() * y) + x);
     }
 
+    function drawName (ctx,name,position) {
+        if(name) {
+            var x = ((position.x * grid_unit_width)/ display_canvas_width)*ctx.canvas.width;
+            var y = ((position.y * grid_unit_height)/ display_canvas_height)*ctx.canvas.height;
+            y = y + 20;
+            x = x + 50;
+            ctx.font = "11px arial";
+            ctx.fillStyle = "grey";
+            ctx.fillText(name, x, y);
+        }
+    }
+
     function drawSquare(ctx,line,position) {
 
         line.from.x = line.from.x -1;
@@ -257,11 +274,29 @@ Meteor.startup(function() {
 
         // if we need to position it in the big display canvas
         if(position){
-            line.from.x = line.from.x + (position.x * (ctx.canvas.width/100));
-            line.to.x = line.to.x + (position.x * (ctx.canvas.width/100));
-            line.from.y = line.from.y + (position.y * (ctx.canvas.height/100));
-            line.to.y = line.to.y + (position.y * (ctx.canvas.height/100));
+
+            console.log(line);
+            line.from.x = line.from.x + ((position.x * grid_unit_width) );
+            line.from.x = (line.from.x / display_canvas_width)*ctx.canvas.width;
+            line.from.y = line.from.y + ((position.y * grid_unit_height) );
+            line.from.y = (line.from.y / display_canvas_height)*ctx.canvas.height;
+
+            line.to.x = line.to.x + ((position.x * grid_unit_width) );
+            line.to.x = (line.to.x / display_canvas_width)*ctx.canvas.width;
+            line.to.y = line.to.y + ((position.y * grid_unit_height) );
+            line.to.y = (line.to.y / display_canvas_height)*ctx.canvas.height;
+
+
+
+            /*
+            console.log(line);
+            line.from.x = line.from.x + (position.x * grid_unit_width);
+            line.to.x = line.to.x + (position.x * grid_unit_width);
+            line.from.y = line.from.y + (position.y * grid_unit_height);
+            line.to.y = line.to.y + (position.y  * grid_unit_height);
             reduc_square_param = reduction_param;
+            */
+            console.log(line);
         }
 
         ctx.beginPath();
@@ -272,7 +307,7 @@ Meteor.startup(function() {
 
         var square = {}
 
-        square.x = (line.from.x + line.to.x)/2
+        square.x = (line.from.x + line.to.x)/2;
         square.y = (line.from.y + line.to.y)/2;
         //console.log(square);
         ctx.fillStyle=line.color1;
@@ -280,6 +315,8 @@ Meteor.startup(function() {
         square.height = line.squareSize1;
         ctx.fillRect(line.from.x,line.from.y,square.width,square.height);
         ctx.stroke();
+
+
         ctx.fillStyle=line.color2;
         square.width = line.squareSize2;
         square.height = line.squareSize2;
@@ -348,17 +385,7 @@ Meteor.startup(function() {
         }
     },false);
 
-    function drawName (ctx,name,position) {
-        if(name) {
-            var x = position.x * (ctx.canvas.width/100);
-            var y = position.y * (ctx.canvas.height/100);
-            y = y + 20;
-            x = x + 50;
-            ctx.font = "11px arial";
-            ctx.fillStyle = "grey";
-            ctx.fillText(name, x, y);
-        }
-    }
+
 
     //Meteor.autorun(function() {
     //    if(!window.mobilecheck()){
--- server/server.js
+++ server/server.js
@@ -1,3 +1,53 @@
+var display_canvas_width = 1200;
+var display_canvas_height = 600;
+var grid_unit_width = 40;
+var grid_unit_height = 40;
+
+function random_scale(a,b) {
+   return Math.floor((Math.random() * b) + a);
+}
+
+function generate_new_grid_position (w,h,treeW,treeH) {
+    // generate a position X Y according to the grid
+    var new_position = {};
+    new_position.x = random_scale(0,w/treeW);
+    new_position.y = random_scale(0,h/treeH);
+    return new_position;
+}
+
+function is_grid_position_available (x,y) {
+    var nb_occurences;
+    nb_occurences = Trees.find(
+        {
+            position_display : {
+                x : x,
+                y : y
+            }
+        }
+    ).count();
+
+    console.log("is_grid_position_available > nb_occurences " + nb_occurences);
+
+    if(nb_occurences > 0) {
+        return false
+    }else{
+        return true
+    }
+}
+
+function set_new_grid_position (w,h,treeW,treeH) {
+    var position_available = false;
+    // while the the position is already taken, generate a new one
+    while (position_available == false) {
+        var new_position = generate_new_grid_position (w,h,treeW,treeH);
+        if(is_grid_position_available (new_position.x,new_position.y)){
+             //if not taken, return the position
+            position_available = true;
+            return new_position;
+        }
+    }
+}
+
 Meteor.publish("trees", function () {
     return Trees.find();
 });
@@ -18,9 +68,11 @@ Meteor.methods({
         }
     },
 	addSquare: function(session_id,square) {
+
         //look for the session id
         var searched_session_tree = Trees.findOne({session_id : session_id});
             //if found,
+            var position = set_new_grid_position (display_canvas_width,display_canvas_height,grid_unit_width,grid_unit_height)
             if(searched_session_tree){
 
                 square.treeId = searched_session_tree._id;
@@ -45,9 +97,12 @@ Meteor.methods({
                 new_session_tree.createdAt =  new Date();
 
                 //give it a place
+                var grid_position = set_new_grid_position (display_canvas_width,display_canvas_height,grid_unit_width,grid_unit_height)
+
                 new_session_tree.position_display = {};
-                new_session_tree.position_display.x = Math.floor((Math.random() * 70) + 10);
-                new_session_tree.position_display.y = Math.floor((Math.random() * 70) + 10);
+                new_session_tree.position_display.x = grid_position.x;
+                new_session_tree.position_display.y = grid_position.y;
+
                 var treeId = Trees.insert(new_session_tree);
                 square.treeId = treeId;
                 Squares.insert(square);
